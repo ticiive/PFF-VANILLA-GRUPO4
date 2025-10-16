d99eb0e06f9ae8d9daea20837668659bc375d50d
gabriel <maccacherog@gmail.com>
2025-10-16 19:22:06 -0300

    ajustes de todas as paginas

--- scriptgaleria.js
+++ scriptgaleria.js
@@ -1,62 +1,45 @@
 const projetos = [
   {
     nomeProjeto: "Projeto sem nome",
-    dataInicio: "1/10/25",
-    dataFim: "29/12/25",
     informacaoProjeto: "O projeto consiste...",
     imagem: "midia/projeto1.jpg"
   },
   {
     nomeProjeto: "App de Estudos",
-    dataInicio: "5/11/25",
-    dataFim: "20/12/25",
     informacaoProjeto: "O projeto consiste...",
     imagem: "midia/projeto2.jpg"
   },
   {
     nomeProjeto: "Site de Receitas",
-    dataInicio: "10/10/25",
-    dataFim: "15/12/25",
     informacaoProjeto: "O projeto consiste...",
     imagem: "midia/projeto3.jpg"
   }
 ];
 
-function criaCard(el){
-    const div = document.getElementById("dv");
-    const card = document.createElement("div");
 
-    card.className = "card";
-    card.style.color = "blue";
-    card.style.border = "1px solid black";
-    card.style.margin = "10px";
-    card.style.padding = "10px";
-
-
-    
-    let imagem = document.createElement("img");
-    imagem.src = el.imagem;
-    
-
-    let nomeProjeto = document.createElement("p");
-    nomeProjeto.textContent = `Nome do Projeto: ${el.nomeProjeto}`;
-
-    let dataInicio = document.createElement("p");
-    dataInicio.textContent = `Data de Início: ${el.dataInicio}`;
-
-    let dataFim = document.createElement("p");
-    dataFim.textContent = `Data de Início: ${el.dataFim}`;
-
-    let informacaoProjeto = document.createElement("p");
-    informacaoProjeto.textContent = `Informações do Projeto: ${el.informacaoProjeto}`;
+  function criaCard(el, index) {
+  const div = document.getElementById("dv");
+  const card = document.createElement("div");
+  card.className = "card";
 
 
 
-    card.append(nomeProjeto,dataInicio,dataFim,informacaoProjeto,imagem);
-    div.append(card);
+  const img = document.createElement("img");
+  img.src = el.imagem;
+  img.alt = `Imagem do ${el.nomeProjeto}`;
+  img.className = "imagem";
 
+  const texto = document.createElement("div");
+  texto.className = "texto";
+  texto.innerHTML = `
+    <h3>${el.nomeProjeto}</h3>
+    <p>${el.informacaoProjeto}</p>
+  `;
 
+  card.append(texto, img);
+  div.append(card);
 }
 
-projetos.forEach(projeto => criaCard(projeto));
+projetos.forEach((projeto, i) => criaCard(projeto, i));
+
 
