1d3ec223b4cacb57089256835821b84dfb3f75d6
gabriel <maccacherog@gmail.com>
2025-10-16 17:55:35 -0300

    algumas correções no js

--- scriptgaleria.js
+++ scriptgaleria.js
@@ -3,19 +3,19 @@ const projetos = [
     nomeProjeto: "Projeto sem nome",
     dataInicio: "1/10/25",
     dataFim: "29/12/25",
-    informacaoProjeto: "O projeto consiste em criar algo muito interessante."
+    informacaoProjeto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod "
   },
   {
     nomeProjeto: "App de Estudos",
     dataInicio: "5/11/25",
     dataFim: "20/12/25",
-    informacaoProjeto: "Aplicativo voltado para organizar estudos e tarefas."
+    informacaoProjeto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"
   },
   {
     nomeProjeto: "Site de Receitas",
     dataInicio: "10/10/25",
     dataFim: "15/12/25",
-    informacaoProjeto: "Um site para compartilhar receitas simples e rápidas."
+    informacaoProjeto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"
   }
 ];
 
@@ -40,7 +40,7 @@ function criaCard(el){
     dataFim.textContent = `Data de Início: ${el.dataFim}`;
 
     let informacaoProjeto = document.createElement("p");
-    informacaoProjeto.textContent = `Data de Início: ${el.informacaoProjeto}`;
+    informacaoProjeto.textContent = `Informações do Projeto: ${el.informacaoProjeto}`;
 
     card.append(nomeProjeto,dataInicio,dataFim,informacaoProjeto);
     div.append(card);
